ed65e3725a9bbc496249d35fb0a6ee7d64cea376
jameswilddev <jameswilddev@outlook.com>
2021-12-09 14:11:26 +0000

    Add events for errors when parsing formatted text.

---
+++ InvalidEscapeSequenceEvent/index.ts
@@ -0,0 +1,32 @@
+/**
+ * An escape sequence is present for a character which is not known to need
+ * escaping.
+ */
+export type InvalidEscapeSequenceEvent = {
+  /**
+   * Identifies the type of event.
+   */
+  readonly type: `incompleteEscapeSequence`;
+
+  /**
+   * The line from which the event originates.
+   */
+  readonly line: number;
+
+  /**
+   * The attempted escape sequence as written in the original document.
+   */
+  readonly verbatim: string;
+
+  /**
+   * The column on which the attempted escape sequence started in the original
+   * document.
+   */
+  readonly fromColumn: number;
+
+  /**
+   * The column on which the attempted escape sequence ended in the original
+   * document.
+   */
+  readonly toColumn: number;
+};
