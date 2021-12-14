8c9ab685d1a58e311d38948944bbec692e270724
jameswilddev <jameswilddev@outlook.com>
2021-12-14 12:06:58 +0000

    Identifier references now include identifier details in-line.

--- IdentifierReference/index.ts
+++ IdentifierReference/index.ts
@@ -3,14 +3,9 @@ import type { Identifier } from "..";
 /**
  * A reference to an identifier on a specific line.
  */
-export type IdentifierReference = {
+export type IdentifierReference = Identifier & {
   /**
    * The line from which the identifier originates.
    */
   readonly line: number;
-
-  /**
-   * The referenced identifier.
-   */
-  readonly identifier: Identifier;
 };
