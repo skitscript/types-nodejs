f535602a40526b10a7bbf0f743363c5a81aff01f
jameswilddev <jameswilddev@outlook.com>
2021-12-15 21:24:16 +0000

    Add column indices to errors and warnings.

--- UnreachableWarning/index.ts
+++ UnreachableWarning/index.ts
@@ -11,4 +11,14 @@ export type UnreachableWarning = {
    * The line from which the warning originates.
    */
   readonly line: number;
+
+  /**
+   * The column on which the warning started in the original document.
+   */
+  readonly fromColumn: number;
+
+  /**
+   * The column on which the warning ended in the original document.
+   */
+  readonly toColumn: number;
 };
