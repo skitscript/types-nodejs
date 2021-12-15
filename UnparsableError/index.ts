0f20f21f48f66b8bc90c492a5b47dd9c4d44326a
siliconspecter <siliconspecter@outlook.com>
2021-12-15 21:24:16 +0000

    Add column indices to errors and warnings.

--- UnparsableError/index.ts
+++ UnparsableError/index.ts
@@ -11,4 +11,14 @@ export type UnparsableError = {
    * The line from which the error originates.
    */
   readonly line: number;
+
+  /**
+   * The column on which the error started in the original document.
+   */
+  readonly fromColumn: number;
+
+  /**
+   * The column on which the error ended in the original document.
+   */
+  readonly toColumn: number;
 };
