0f24332507e37b9316189edbb05910cc4fafe6e4
siliconspecter <siliconspecter@outlook.com>
2021-12-18 14:52:17 +0000

    Add character states.

---
+++ NonPresentInterpreterStateCharacterState/index.ts
@@ -0,0 +1,9 @@
+/**
+ * A character is currently not visible.
+ */
+export type NonPresentInterpreterStateCharacterState = {
+  /**
+   * Identifies the type of character state.
+   */
+  readonly type: `nonPresent`;
+};
---
+++ PresentInterpreterStateCharacterState/index.ts
@@ -0,0 +1,9 @@
+/**
+ * A character is currently visible, and is not animating.
+ */
+export type PresentInterpreterStateCharacterState = {
+  /**
+   * Identifies the type of character state.
+   */
+  readonly type: `present`;
+};
