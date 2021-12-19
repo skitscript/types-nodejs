eaccf7f008f18589cdd084441ddf4257d779cbec
jameswilddev <jameswilddev@outlook.com>
2021-12-19 12:48:48 +0000

    Add interpreter error states.

---
+++ InfiniteLoopInterpreterStateError/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Interpreting failed as an infinite loop which does not contain user interaction was found.
+ */
+export type InfiniteLoopInterpreterStateError = {
+  /**
+   * Indicates the type of interpreter error.
+   */
+  readonly type: `infiniteLoop`;
+};
