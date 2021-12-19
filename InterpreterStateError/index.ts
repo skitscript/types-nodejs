407b6b6c9ce3dc58b4685de46cff28114e200532
siliconspecter <siliconspecter@outlook.com>
2021-12-19 12:48:48 +0000

    Add interpreter error states.

---
+++ InterpreterStateError/index.ts
@@ -0,0 +1,6 @@
+import type { InfiniteLoopInterpreterStateError } from "../InfiniteLoopInterpreterStateError";
+
+/**
+ * An error which can be encountered while interpreting a parsed document.
+ */
+export type InterpreterStateError = InfiniteLoopInterpreterStateError;
---
+++ InvalidInterpreterState/index.ts
@@ -0,0 +1,16 @@
+import type { InterpreterStateError } from "../InterpreterStateError";
+
+/**
+ * The result of unsuccessfully interpreting a parsed document.
+ */
+export type InvalidInterpreterState = {
+  /**
+   * Indicates the type of result.
+   */
+  readonly type: `invalid`;
+
+  /**
+   * The error which was encountered.
+   */
+  readonly error: InterpreterStateError;
+};
